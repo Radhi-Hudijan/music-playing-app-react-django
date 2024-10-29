a779ec4b5fba02ff0230259332c9e89cb018c3b0
Radhi Hudijan <radhi.hudijan@accenture.com>
2024-10-29 15:06:44 +0100

    Add react-router-dom dependency and update package-lock.json

---
+++ client/music-app/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h1>Home</h1>
+        <p>Welcome to the Music App!</p>
+      </div>
+    </div>
+  );
+}
--- client/music-app/vite.config.js
+++ client/music-app/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       // proxy requests to the server
+//       "/api": {
+//         target: "http://localhost:3000",
+//         secure: false,
+//       },
+//     },
+//     watch: {
+//       usePolling: true,
+//     },
+//   },
+// })
